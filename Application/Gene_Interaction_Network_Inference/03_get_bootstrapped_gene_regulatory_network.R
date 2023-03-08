9b674fc381deba9ed69d58ff74e8677b4002963c
LiangyingYin <34902553+LiangyingYin@users.noreply.github.com>
2023-03-08 10:47:39 +0800

    Update 03_get_bootstrapped_gene_regulatory_network.R

--- Application/Gene_Interaction_Network_Inference/03_get_bootstrapped_gene_regulatory_network.R
+++ Application/Gene_Interaction_Network_Inference/03_get_bootstrapped_gene_regulatory_network.R
@@ -1,7 +1,15 @@
 
+#*****************************************
 # Written by yinly, May 11
+#*****************************************
 
-#
+#*******************************************************************************************
+# Revised by: yinly, Oct 12, 2020
+# Description: update the pMax matrix derived from pc_obj accordingly.
+# When apply stable.fast for the pc algorithm, the pMax matrix is only upper-triangular updated 
+# while estimating the skeleton. In this regard, we could update the pMax matrix accordingly 
+# based on the adjacency matrix
+#*******************************************************************************************
 # #***************************************
 # #  pc algorithm applied to GTEx
 # #***************************************
@@ -14,178 +22,7 @@ library(coop)
 library(pcalg)
 library(parallel)
 library(glasso)
-
-bootstrap_pc_network<-function(samples_data_bootstrap,times_bootstrap,cut_off)
-{
-  adj_list = list()
-  adj_final = NULL
-  p_max_final = NULL
-  p_max_list = list()
-  for(i in 1:times_bootstrap)
-  {
-    new_sample_index = sample(seq(1,nrow(samples_data_bootstrap)), nrow(samples_data_bootstrap),replace = TRUE)
-    new_samples = samples_data_bootstrap[new_sample_index,]
-    # if(i==1) #or randomly choose
-    # {
-    #   save(new_samples, file=paste0(fold_path,"new_samples.Rdata"))
-    # }
-    corr_mat = pcor(new_samples) 
-    suffStat <- list(C=corr_mat ,n=nrow(new_samples))
-    alpha = 0.01   #alpha (p-value) threshold for pc function
-    numCores = 16  #number of cores for stable.fast algo in pcalg
-    m.max = 4      #max. number of conditioning variables
-    pc_obj = pc(suffStat,
-                indepTest=gaussCItest,
-                p=ncol(corr_mat),
-                skel.method="stable.fast",
-                #fixedGaps = FixedGaps_mat,
-                alpha = alpha,
-                numCores = numCores ,
-                m.max = m.max ,  #ref: reduced PC paper https://arxiv.org/abs/1806.06209
-                verbose= FALSE)
-    # pc_obj = PC_Algorithm_GeneNetwork(new_samples)
-    # ## estimate CPDAG and PDAG -- see  help(pc)
-    # suffStat <- list(C = cor(new_samples), n = sample_num)
-    # pc_obj <- pc(suffStat, indepTest = gaussCItest, p = node_num, alpha = 0.01)
-    adj = as(pc_obj@graph,"matrix")
-    adj_list[[i]] = adj
-    
-    p_max_list[[i]] = pc_obj@pMax
-    
-    if(i==1){adj_final = adj}
-    else{ adj_final = adj_final + adj }
-  }
-  
-  adj_final_output = adj_final
-  library(popbio)
-  p_max_final = mean.list(p_max_list) # initial value for p_max_final
-  non_zero_location = which(adj_final!=0,arr.ind = T)
-  rows = dim(non_zero_location)[1] #num of row
-  columns = dim(non_zero_location)[2] #num of column
-  for (i in 1:rows)
-  {
-    x_loc = non_zero_location[i,1]
-    y_loc = non_zero_location[i,2]
-    edge_mark = adj_final[x_loc,y_loc]
-    if(edge_mark>=times_bootstrap*cut_off)
-    {
-      adj_final_output[x_loc,y_loc] = 1
-    }
-    else
-    { 
-      adj_final_output[x_loc,y_loc] = 0 
-      p_max_final[x_loc,y_loc] = -1
-    }
-  }
-  graph_final_est = as(adj_final_output,"graphNEL")
-  
-  
-  # for edge we final remain, calculate value for each of them
-  non_zero_loc_edge_final = which(adj_final_output!=0,arr.ind = T)
-  rows2 = dim(non_zero_loc_edge_final)[1] #num of row
-  columns2 = dim(non_zero_loc_edge_final)[2] #num of column
-  
-  for (i in 1:rows2)
-  {
-    x_loc = non_zero_loc_edge_final[i,1]
-    y_loc = non_zero_loc_edge_final[i,2]
-    tmp_list_pvalue = list()
-    tmp_count = 1 # count for tmp_list_pvalue
-    for(j in 1:length(adj_list))
-    {
-      adj_list_each = adj_list[[j]]
-      edge_mark = adj_list_each[x_loc,y_loc]
-      if(edge_mark==1)
-      {
-        tmp_list_pvalue[[tmp_count]] = p_max_list[[j]][x_loc,y_loc]
-        tmp_count = tmp_count+1
-      }
-    }
-    p_max_final[x_loc,y_loc] = mean(unlist(tmp_list_pvalue)) 
-  }
-  
-  bootstrap_result_list[[1]] = graph_final_est
-  bootstrap_result_list[[2]] = p_max_final
-  
-  return(bootstrap_result_list)
-}
-
-bootstrap_pc_network2<-function(samples_data_bootstrap,times_bootstrap,cut_off)
-{
-  adj_list = list()
-  adj_final = NULL
-  p_max_final = NULL
-  p_max_list = list()
-  for(i in 1:times_bootstrap)
-  {
-    new_sample_index = sample(seq(1,nrow(samples_data_bootstrap)), nrow(samples_data_bootstrap),replace = TRUE)
-    new_samples = samples_data_bootstrap[new_sample_index,]
-    corr_mat = pcor(new_samples) 
-    suffStat <- list(C=corr_mat ,n=nrow(new_samples))
-    alpha = 0.01   #alpha (p-value) threshold for pc function
-    numCores = 16  #number of cores for stable.fast algo in pcalg
-    m.max = 4      #max. number of conditioning variables
-    pc_obj = pc(suffStat,
-                indepTest=gaussCItest,
-                p=ncol(corr_mat),
-                skel.method="stable.fast",
-                #fixedGaps = FixedGaps_mat,
-                alpha = alpha,
-                numCores = numCores ,
-                m.max = m.max ,  #ref: reduced PC paper https://arxiv.org/abs/1806.06209
-                verbose= FALSE)
-    # pc_obj = PC_Algorithm_GeneNetwork(new_samples)
-    # ## estimate CPDAG and PDAG -- see  help(pc)
-    # suffStat <- list(C = cor(new_samples), n = sample_num)
-    # pc_obj <- pc(suffStat, indepTest = gaussCItest, p = node_num, alpha = 0.01)
-    adj = as(pc_obj@graph,"matrix")
-    adj_list[[i]] = adj
-    # p_max_list[[i]] = pc_obj@pMax
-    
-    adj_tmp = adj
-    pMax_tmp = pc_obj@pMax
-    pMax_new = pMax_tmp*adj_tmp
-    pMax_new[adj==0]=-1
-    p_max_list[[i]] = pMax_new
-    if(i==1){
-      adj_final = adj
-    }
-    else{ 
-      adj_final = adj_final + adj 
-      # p_max_final = p_max_final+pMax_new
-      # p_max_list[[i]] = pc_obj@pMax
-    }
-  }
-  
-  adj_final_output = adj_final
-  library(popbio)
-  p_max_final = mean.list(p_max_list) # initial value for p_max_final
-  non_zero_location = which(adj_final!=0,arr.ind = T)
-  rows = dim(non_zero_location)[1] #num of row
-  columns = dim(non_zero_location)[2] #num of column
-  for (i in 1:rows)
-  {
-    x_loc = non_zero_location[i,1]
-    y_loc = non_zero_location[i,2]
-    edge_mark = adj_final[x_loc,y_loc]
-    if(edge_mark>=times_bootstrap*cut_off)
-    {
-      adj_final_output[x_loc,y_loc] = 1
-    }
-    else
-    { 
-      adj_final_output[x_loc,y_loc] = 0 
-      p_max_final[x_loc,y_loc] = -1
-      #cat("This is p_max_final",p_max_final[x_loc,y_loc],"\n")
-    }
-  }
-  graph_final_est = as(adj_final_output,"graphNEL")
-  
-  bootstrap_result_list[[1]] = graph_final_est
-  bootstrap_result_list[[2]] = p_max_final
-  
-  return(bootstrap_result_list)
-}
+library(Matrix)
 
 bootstrap_pc_network3<-function(samples_data_bootstrap,times_bootstrap,cut_off)
 {
@@ -218,12 +55,28 @@ bootstrap_pc_network3<-function(samples_data_bootstrap,times_bootstrap,cut_off)
                 skel.method="stable.fast",
                 #fixedGaps = FixedGaps_mat,
                 alpha = alpha,
+                #n2pd = "Retry",
                 numCores = numCores ,
                 m.max = m.max ,  #ref: reduced PC paper https://arxiv.org/abs/1806.06209
                 verbose= FALSE)
     # ## estimate CPDAG and PDAG -- see  help(pc)
     # suffStat <- list(C = cor(new_samples), n = sample_num)
     # pc_obj <- pc(suffStat, indepTest = gaussCItest, p = node_num, alpha = 0.01)
+    #***********************************************************************************
+    # Revised by yinly, Oct 12, 2020
+    # Description: update the pMax matrix derived from the pc algorithm accordingly
+    #***********************************************************************************
+    pMax_temp = pc_obj@pMax
+    pMax_temp = forceSymmetric(pMax_temp,uplo="U")
+    pMax_update = pMax_temp
+    pMax_update[which(pMax_update==-1)] = 1
+    adj_temp = as(pc_obj@graph,"matrix")
+    #pMax_update[which(pMax_update<alpha & adj_temp ==0)] = 1  
+    #pMax_final = Matrix(pMax_update,nrow=nrow(pMax_update))
+    pMax_final = as.matrix(pMax_update)
+    pc_obj@pMax = pMax_final
+    # end of revision
+    
     adj = as(pc_obj@graph,"matrix")
     
     adj_list[[i]] = adj
@@ -297,27 +150,31 @@ bootstrap_pc_network3<-function(samples_data_bootstrap,times_bootstrap,cut_off)
 # infer the gene-gene network with select genes by bootstrapped pc algorithm 
 #***************************************************************************************
 # The file directory for resid_mat with selected genes
-resid_mat_selected_prefix = "/exeh_3/yinly/BayesianNetwork/02_GTex_Network/01_Single_Tissue/Residual_Results_PCSelect_Based/"
-#resid_mat_selected_prefix = "/exeh_3/yinly/BayesianNetwork/02_GTex_Network/01_Single_Tissue/Residual_Result_Selected/"
+#resid_mat_selected_prefix = "/exeh_3/yinly/BayesianNetwork/02_GTex_Network/01_Single_Tissue/Residual_Results_PCSelect_Based/"
+resid_mat_selected_prefix = "/exeh_3/yinly/BayesianNetwork/02_GTex_Network/01_Single_Tissue/Residual_Result_Selected_CIS_Trans/"
 
 files = list.files(path=resid_mat_selected_prefix)
+#index = c(1,5,11,12,15,21,22,27,29,32,41)
 # Iterative all files in the directory
 for(i in 1:length(files)){
-  #*************************************
-  #  Step 1: load residual_mat 
-  #*************************************
-  filepath = paste0(resid_mat_selected_prefix,files[i])
-  load(filepath) ## object: resid_mat_selected
-  
-  #*************************************
-  #  Step 2: run bootstrapped pc 
-  #*************************************
-  bootstrap_result_list = list()
-  pc_graph = bootstrap_pc_network3(resid_mat_selected,100,0.6)
-  graph_est = pc_graph[[1]]
-  pc_graph_prefix = "/exeh_3/yinly/BayesianNetwork/02_GTex_Network/01_Single_Tissue/GTEx_PCSelect/"
-  file_name = gsub("_resid_mat_selected.Rdata","",files[i])
-  save(pc_graph,file = paste0(pc_graph_prefix,file_name,"_Causal_Network_PCSelect.Rdata"))
+#for(j in 1:length(index)){
+ #i = index[j]
+ #*************************************
+ #  Step 1: load residual_mat 
+ #*************************************
+ #file = "Whole_Blood_CAD_resid_mat_selected.Rdata"
+ filepath = paste0(resid_mat_selected_prefix,files[i])
+ load(filepath) ## object: resid_mat_selected
+
+ #*************************************
+ #  Step 2: run bootstrapped pc 
+ #*************************************
+ bootstrap_result_list = list()
+ pc_graph = bootstrap_pc_network3(resid_mat_selected,100,0.6)
+ graph_est = pc_graph[[1]]
+ pc_graph_prefix = "/exeh_3/yinly/BayesianNetwork/02_GTex_Network/01_Single_Tissue/GTEx_PCSelect_CIS_Trans/"
+ file_name = gsub("_resid_mat_selected.Rdata","",files[i])
+ #file_name = gsub("_resid_mat_selected.Rdata","",file)
+ save(pc_graph,file = paste0(pc_graph_prefix,file_name,"_Causal_Network_PCSelect.Rdata"))
+ cat("The bootstrapped pc algorithm for the",i,"trait",file_name,"is completed! \n")
 }
-
-
